2ce93cb0112253fac34bc780fdf1a7943c004c19
guigalvao123 <guilherme.galvaopadua@gmail.com>
2022-10-03 13:22:52 -0300

    Exercicios da aula a027 corrigidos

---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+let valorDoInput = document.getElementById("nome")
+valorDoInput.value = prompt("Adicione seu nome")
+
+let valorDoEndereco = document.getElementById("endereco")
+valorDoEndereco.value = prompt("Adicione seu endereco")
+
+let valorDoEmail = document.getElementById("email")
+valorDoEmail.value = prompt("Adicione seu email")
